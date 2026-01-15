95e78d9923ea107f54b4655b20e35b3418742bf6
Eric Vieira <ericvieira.p@outlook.com>
2026-01-15 20:06:39 -0300

    Exercicio proposto sobre hashcode

---
+++ java-ex-curso/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import model.entities.Usuario;
+
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Set<Usuario> alunos = new HashSet<>();
+
+        System.out.print("How many students for course A? ");
+        lerAlunos(sc, alunos);
+
+        System.out.print("How many students for course B? ");
+        lerAlunos(sc, alunos);
+
+        System.out.print("How many students for course C? ");
+        lerAlunos(sc, alunos);
+
+        System.out.println("Total students: " + alunos.size());
+
+        sc.close();
+    }
+
+    private static void lerAlunos(Scanner sc, Set<Usuario> alunos) {
+        int qtd = sc.nextInt();
+        for (int i = 0; i < qtd; i++) {
+            alunos.add(new Usuario(sc.nextInt()));
+        }
+    }
+}
---
+++ java-ex-curso/src/model/entities/Usuario.java
@@ -0,0 +1,28 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Usuario {
+
+    private int id;
+
+    public Usuario(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
